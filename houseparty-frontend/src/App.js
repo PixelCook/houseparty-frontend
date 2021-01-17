2d527fc0c75e279e767ae0252e23fae1d03fa2fc
PixelCook <zacharyigould@gmail.com>
2021-01-17 12:52:54 +0200

    conditionals

--- houseparty-frontend/src/App.js
+++ houseparty-frontend/src/App.js
@@ -1,16 +1,15 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import Home from "./pages/homepage"
-import Signin from './modules/Signin'
-import Login from './modules/Login'
-import LogOut from './modules/LogOut';
+import Home from "./pages/homepage";
+import Signin from "./modules/Signin";
+import Login from "./modules/Login";
+import LogOut from "./modules/LogOut";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 
 function App() {
@@ -18,12 +17,11 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path='/login' component={Login} />
-          <Route path='/logout' component={LogOut} />
-          <Route path='/signin' component={Signin} />
-          <Redirect to='/home' />
+          <Route path="/login" component={Login} />
+          <Route path="/logout" component={LogOut} />
+          <Route path="/signin" component={Signin} />
+          <Redirect to="/home" />
         </Switch>
-
       </Router>
     </div>
   );
