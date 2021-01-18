771fed930840b97ae17a7ee0c860446df04185e3
DanielaSmid17 <dani.smid@gmail.com>
2021-01-18 17:32:19 +0200

    create a party basic func implemented

--- src/modules/StartParty.jsx
+++ src/modules/StartParty.jsx
@@ -1,26 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import startparty from "../DesignImages/2.svg";
 import { Grid, Button } from '@material-ui/core';
 import '../CSS/startparty.css'
+import userContext from '../context/userContext'
+import axios from 'axios'
+import { startPartyUrl } from '../utils/config'
 
 
 export default function StartParty() {
   const date = new Date();
+  const [startPartyValues, setStartPartyValues] = useState({})
   const [selectedDate, setSelectedDate] = useState(date.toISOString().split('T')[0]);
+  const [partyCreated, setPartyCreated] = useState('')
+
+  const { user } = useContext(userContext)
 
 
   const handleDateChange = (e) => {
+    const startPartyValuesCopy = { ...startPartyValues }
     console.log(e.target.value);
-    setSelectedDate(e.target.value);
+    const { value, name } = e.target
+    startPartyValuesCopy['userId'] = user.id
+    startPartyValuesCopy[name] = value
+    setSelectedDate(value);
+    setStartPartyValues(startPartyValuesCopy)
+
   }
 
-  const handleCreateParty = (e) => {
+  const handleCreateParty = async (e) => {
     e.preventDefault()
-
+    try {
+      const response = await axios.post(startPartyUrl, startPartyValues)
+      const partyId = response.data
+      setPartyCreated(partyId)
+      console.log(partyCreated);
+    }
+    catch (err) {
+      console.log(err);
+    }
   }
 
   return (
-    <Grid container justify="space-around" className='startaparty-wrapper'>
+    <Grid container justify="space-around" spacing={3}>
       <div
         className="background"
         style={{
@@ -29,14 +50,15 @@ export default function StartParty() {
           height: "100vh",
           backgroundSize: "100% 100%",
         }}
-      ><div>
+      >
+        <div>
           <h1 className="startaparty" style={{ color: "#F90040" }}>Throw A Party</h1>
           <p className="startaparty-msg" > A party always starts when you're here, take the lead</p>
         </div>
         <h3 className="startaparty-when">When is your Party happening?</h3>
         <div className="startaparty-form">
           <Grid item xs={11}>
-            <input type="date" id="start" name="trip-start"
+            <input type="date" id="start" name="date"
               value={selectedDate}
               min={date.toISOString().split('T')[0]}
               onChange={handleDateChange}
@@ -45,7 +67,13 @@ export default function StartParty() {
           <Button variant="contained" color="secondary" onClick={handleCreateParty}>
             Create Party</Button>
         </div>
+        {partyCreated &&
+          <div className='createdParty'> You have created a party! It's ID number is
+              <div className='partyId'>
+              {partyCreated}
+            </div>
+          </div>}
       </div>
-    </Grid>
+    </Grid >
   );
 }
--- src/utils/config.js
+++ src/utils/config.js
@@ -2,3 +2,4 @@ const baseUrl = 'http://localhost:5000/'
 
 export const signUpUrl = baseUrl + 'signup'
 export const loginUrl = baseUrl + 'login'
+export const startPartyUrl = baseUrl + 'startparty'
