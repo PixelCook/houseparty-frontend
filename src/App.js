540067ff4bf3fb239a421fc45d50e5f6134e45fa
Inon Bercu <inonbr@gmail.com>
2021-01-18 16:34:22 +0200

    after signin and login, redirect to spotify login -> after login spotify token stored as localstorage and redirect to home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import Home from "./pages/homepage";
-import Signin from "./modules/Signin";
-import Login from "./modules/Login";
-import LogOut from "./modules/LogOut";
-import UserContext from "./context/userContext";
-import jwt_decode from "jwt-decode";
-import Navbar from "./modules/Navbar";
-import StartParty from "../src/modules/StartParty";
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Home from './pages/homepage';
+import Signin from './modules/Signin';
+import Login from './modules/Login';
+import LogOut from './modules/LogOut';
+import UserContext from './context/userContext';
+import jwt_decode from 'jwt-decode';
+import Navbar from './modules/Navbar';
+import StartParty from '../src/modules/StartParty';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
   Redirect,
-} from "react-router-dom";
+} from 'react-router-dom';
 
 function App() {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem('token');
     if (token) {
       const decoded = jwt_decode(token);
       setUser(decoded);
@@ -28,18 +28,18 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className='App'>
       <UserContext.Provider value={{ user, setUser }}>
         <Navbar />
         <Router>
           <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/connect" />
-            <Route path="/login" component={Login} />
-            <Route path="/logout" component={LogOut} />
-            <Route path="/signin" component={Signin} />
-            <Route path="/start-party" component={StartParty} />
-            <Redirect to="/home" />
+            <Route path='/home' component={Home} />
+            <Route path='/connect' />
+            <Route path='/login' component={Login} />
+            <Route path='/logout' component={LogOut} />
+            <Route path='/signin' component={Signin} />
+            <Route path='/start-party' component={StartParty} />
+            <Redirect to='/home' />
           </Switch>
         </Router>
       </UserContext.Provider>
