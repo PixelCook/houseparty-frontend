dd9d73bb6f7ddfceb20b68b809b33f557a20296a
DanielaSmid17 <dani.smid@gmail.com>
2021-01-19 16:47:53 +0200

    join a party basic func implemented

--- src/modules/JoinParty.jsx
+++ src/modules/JoinParty.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "../CSS/home.css";
 import Join from "../DesignImages/3.svg";
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  Button,
+  Grid
+} from '@material-ui/core';
+import '../CSS/joinparty.css'
+import axios from 'axios'
+import { joinPartyUrl } from '../utils/config'
 
 export default function JoinParty() {
+  const [joinPartyValue, setJoinPartyValue] = useState({})
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const joinPartyValueCopy = { ...joinPartyValue };
+    joinPartyValueCopy[name] = value;
+
+    setJoinPartyValue(joinPartyValueCopy)
+  }
+
+  const handleJoinParty = async () => {
+    try {
+
+      console.log('before sending', joinPartyValue);
+      const response = await axios.post(joinPartyUrl, joinPartyValue)
+      const partyJoined = await response.data
+      localStorage.setItem('partyId', partyJoined.partyId)
+      window.location = '/home'
+    } catch (err) {
+      alert(err.response.data);
+    }
+  }
+
+
   return (
     <div
       className="background"
@@ -12,6 +46,36 @@ export default function JoinParty() {
         height: "100vh",
         backgroundSize: "100% 100%",
       }}
-    ></div>
+    >
+      <div className='joinparty-wrapper'>
+        <Grid container>
+          <Grid item xs={11}>
+            <h4 className='joinparty-ready'>Ready to Join a Party?</h4>
+          </Grid>
+          <Grid item xs={11}>
+            <FormControl>
+              <InputLabel htmlFor='my-input'>Party ID</InputLabel>
+              <Input
+                name='partyId'
+                id='my-input'
+                aria-describedby='my-helper-text'
+                onChange={handleChange}
+              />
+            </FormControl>
+          </Grid>
+          <Grid item xs={11}>
+            <Button
+              variant='contained'
+              color='secondary'
+              onClick={handleJoinParty}
+            >
+              Join
+          </Button>
+          </Grid>
+
+        </Grid>
+      </div>
+
+    </div>
   );
 }
--- src/utils/config.js
+++ src/utils/config.js
@@ -3,3 +3,4 @@ const baseUrl = 'http://localhost:5000/';
 export const signUpUrl = baseUrl + 'signup'
 export const loginUrl = baseUrl + 'login'
 export const startPartyUrl = baseUrl + 'startparty'
+export const joinPartyUrl = baseUrl + 'joinparty'
