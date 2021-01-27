141eab9d63233570e5528b0a319945a4c43d165d
DanielaSmid17 <dani.smid@gmail.com>
2021-01-27 14:30:00 +0200

    push again

--- src/components/SearchDisplay.jsx
+++ src/components/SearchDisplay.jsx
@@ -51,8 +51,8 @@ export default function TitlebarGridList(props) {
                 actionIcon={
                   <IconButton
                     aria-label={`star ${results.album.name}`}
-                    onClick={() => {
-                      addSong(party.playlistId, results.id);
+                    onClick={async () => {
+                      await addSong(party.playlistId, results.id);
                     }}
                   >
                     <StarBorderIcon className={classes.title} />
